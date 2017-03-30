21aa35e46cdedd3d0d7afc835dff13d71befacfc
Marc Creutzberg <mail@mcreutzberg.dk>
2017-03-30 09:41:33 +0200

    Fixing bug there enenys was bugging at wall

--- app/src/main/java/org/example/canvasdemo/MyView.java
+++ app/src/main/java/org/example/canvasdemo/MyView.java
@@ -14,27 +14,92 @@ import java.util.Random;
 
 public class MyView extends View {
 
+    //Setting Bitmaps for the 3 Icons. Pac Icon, Coin Icon and Enemy Icon
     Bitmap pacIcon = BitmapFactory.decodeResource(getResources(), R.drawable.pacman);
     Bitmap coinIcon = BitmapFactory.decodeResource(getResources(), R.drawable.coin_1x);
     Bitmap enemyIcon = BitmapFactory.decodeResource(getResources(), R.drawable.red);
 
+    //Setting 2 list for goldCoins and Enemys
     ArrayList<GoldCoin> goldCoins;
     ArrayList<Enemy> enemies;
+
+    //Createing variable for points
     int points = 0;
 
-    public void setGoldCoint(ArrayList<GoldCoin> gold) {
-        goldCoins = gold;
-    }
-
-    public void setEnemies(ArrayList<Enemy> enemies) {
-        this.enemies = enemies;
-    }
-
-    //The coordinates for our dear pacman: (0,0) is the top-left corner
+    //The coordinates for our pacman: (0,0) is the top-left corner
     int pacx = 50;
     int pacy = 400;
-    int h, w; //used for storing our height and width
 
+    //Height and Width of the canvas
+    int h, w;
+
+    /* The next 3 constructors are needed for the Android view system,
+    when we have a custom view.
+     */
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * Drawing the view - painting pac, all goldCoins, all Enemys and setteing H and W of the canvas
+     * @param canvas
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //Here we get the height and weight
+        h = canvas.getHeight();
+        w = canvas.getWidth();
+
+        //Making a new paint object
+        Paint paint = new Paint();
+        canvas.drawColor(Color.WHITE); //clear entire canvas to white color
+
+        canvas.drawBitmap(pacIcon, pacx, pacy, paint);
+
+        //canvas.drawRect(r, paint);
+        for (GoldCoin g : goldCoins) {
+            if (!g.isTaken()) {
+                if (!g.isInitialized()) {
+                    g.setX(randomMiser(0, w - 100));
+                    g.setY(randomMiser(0, h - 100));
+                    g.setInitialized(true);
+                }
+                canvas.drawBitmap(coinIcon, g.getX(), g.getY(), paint);
+            }
+        }
+
+        for (Enemy e : enemies) {
+            if (!e.isInitialized()) {
+                e.setX(randomMiser(0, w - enemyIcon.getWidth()));
+                e.setY(randomMiser(0, h - enemyIcon.getHeight()));
+                
+                //if enemy is spawing on pac at game start, then enemy will get a new position
+                while (spaceBetween(pacx, pacy, e.getX(), e.getY()) < 100){
+                    e.setX(randomMiser(0, w - enemyIcon.getWidth()));
+                    e.setY(randomMiser(0, h - enemyIcon.getHeight()));
+                }
+
+            }
+            canvas.drawBitmap(enemyIcon, e.getX(), e.getY(), paint);
+        }
+
+        super.onDraw(canvas);
+    }
+
+    /**
+     * Moving pac
+     * @param way can be 1,2,3,4 descriptions the way the enemy should move
+     * @param movingPixels
+     * @return
+     */
     public int movePac(int way, int movingPixels) {
         if (way == 0) {
             //up og ned
@@ -56,6 +121,10 @@ public class MyView extends View {
         return points;
     }
 
+    /**
+     * Moving all enemys 5 px.
+     * @param counter
+     */
     public void moveEnemy(int counter) {
         int movingEnemyPixels = 5;
         for (Enemy e : enemies) {
@@ -89,62 +158,10 @@ public class MyView extends View {
         }
     }
 
-    /* The next 3 constructors are needed for the Android view system,
-    when we have a custom view.
+
+    /**
+     * Cheching if pac is on a conin and counting points
      */
-    public MyView(Context context) {
-        super(context);
-
-    }
-
-    public MyView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-
-    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    //In the onDraw we put all our code that should be
-    //drawn whenever we update the screen.
-    @Override
-    protected void onDraw(Canvas canvas) {
-        //Here we get the height and weight
-        h = canvas.getHeight();
-        w = canvas.getWidth();
-
-        //Making a new paint object
-        Paint paint = new Paint();
-        canvas.drawColor(Color.WHITE); //clear entire canvas to white color
-
-        canvas.drawBitmap(pacIcon, pacx, pacy, paint);
-
-        //canvas.drawRect(r, paint);
-        for (GoldCoin g : goldCoins) {
-            if (!g.isTaken()) {
-                if (!g.isInitialized()) {
-                    g.setX(randomMiser(0, w - 100));
-                    g.setY(randomMiser(0, h - 100));
-                    g.setInitialized(true);
-                }
-                canvas.drawBitmap(coinIcon, g.getX(), g.getY(), paint);
-            }
-        }
-
-        for (Enemy e : enemies) {
-            if (!e.isInitialized()) {
-                e.setX(randomMiser(0, w - 100));
-                e.setY(randomMiser(0, h - 100));
-                e.setInitialized(true);
-            }
-            canvas.drawBitmap(enemyIcon, e.getX(), e.getY(), paint);
-        }
-
-        super.onDraw(canvas);
-    }
-
-
     public void isOnCoin() {
         for (GoldCoin g : goldCoins) {
             if (!g.isTaken()) {
@@ -156,6 +173,9 @@ public class MyView extends View {
         }
     }
 
+    /**
+     * Cheching if pac is on a enemy then resetting the game
+     */
     public void isOnEnemy() {
         for (Enemy e : enemies) {
             if (spaceBetween(pacx, pacy, e.getX(), e.getY()) < 100) {
@@ -185,8 +205,19 @@ public class MyView extends View {
         }
 
         for (Enemy e : enemies) {
-            e.setX(randomMiser(0, w - 100));
-            e.setY(randomMiser(0, h - 100));
+            e.setX(randomMiser(0, w - enemyIcon.getWidth()));
+            e.setY(randomMiser(0, h - enemyIcon.getHeight()));
+
+
+            //
+            // // HEERRR
+            //
+            //if enemy is spawing on pac at game start, then enemy will get a new position
+            while (spaceBetween(pacx, pacy, e.getX(), e.getY()) < 100){
+                e.setX(randomMiser(0, w - enemyIcon.getWidth()));
+                e.setY(randomMiser(0, h - enemyIcon.getHeight()));
+            }
+
             e.setInitialized(true);
         }
 
@@ -217,6 +248,14 @@ public class MyView extends View {
     public static int randomMiser(int min, int max) {
         Random rand = new Random();
         return rand.nextInt(max - min + 1) + min;
+    }
+
+    public void setGoldCoint(ArrayList<GoldCoin> gold) {
+        goldCoins = gold;
+    }
+
+    public void setEnemies(ArrayList<Enemy> enemies) {
+        this.enemies = enemies;
     }
 
 }
